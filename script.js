a57251033072843f9fbf7d0b84d6638b2984d834
Spencertainty <sjmfeagin@gmail.com>
2024-05-15 12:15:59 -0600

    changed 1 to i on line 5 of js

--- script.js
+++ script.js
@@ -2,10 +2,12 @@ const sketch = document.getElementById('sketch');
 
 function createGrid(size) {
     sketch.innerHTML = '';
-    for (let i = 0; i < size * size; 1++) {
+    for (let i = 0; i < size * size; i++) {
         const cell = document.createElement('div');
         sketch.appendChild(cell);
     }
 }
 
 createGrid(16);
+
+console.log
