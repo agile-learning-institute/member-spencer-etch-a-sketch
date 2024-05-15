3040cdcb63fa305d0caa05f692848a036dff0f7b
Spencertainty <sjmfeagin@gmail.com>
2024-05-15 13:07:45 -0600

    added a color-picker feature tohtml and js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+let currentColor = 'grey';
+
 const sketch = document.getElementById('sketch');
+
+document.getElementById('color-picker').addEventListener('change', function(event) {
+    currentColor = event.target.value;
+});
 
 function createGrid(size) {
     sketch.innerHTML = '';
     for (let i = 0; i < size * size; i++) {
         const cell = document.createElement('div');
+        cell.addEventListener('mouseover', function() {
+            cell.style.backgroundColor = currentColor;
+        });
         sketch.appendChild(cell);
     }
 }
